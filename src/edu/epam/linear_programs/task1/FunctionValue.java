71104cc4ccf54f2d0e928108f160d056706a2757
Tzeentche <ilyasugako87@gmail.com>
2019-07-13 20:28:33 +0300

    Initial commit

---
+++ src/edu/epam/linear_programs/task1/FunctionValue.java
@@ -0,0 +1,34 @@
+package edu.epam.linear_programs.task1;
+
+//  Найдите значение функции: z = ( (a – 3 ) * b / 2) + c
+
+import java.util.*;
+
+public class FunctionValue {
+
+    public static void main(String[] args) {
+
+        double result;
+        double[] param = new double[3];
+        char[] val = {'A', 'B', 'C'};
+
+        Scanner input = new Scanner(System.in);
+
+        try {
+
+            for (int i = 0; i < param.length; i ++) {
+
+                System.out.println("For equation \"z = ((a - 3) * b / 2) + c\", please, enter numeral value " + val[i] + ": ");
+                param[i] = input.nextDouble();
+            }
+
+            result = ((param[0] - 3) * param[1] / 2) + (param[2]);
+            System.out.println("Function value is " + result);
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Genius, enter NUMERAL value! Try again.");
+        }
+    }
+
+}
