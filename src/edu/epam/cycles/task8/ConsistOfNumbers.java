ad549ba97e77705cc5089f939f6c8051e9d4b93c
Tzeentche <ilyasugako87@gmail.com>
2019-07-19 00:22:28 +0300

    Refactoring decision for task 1.3.8

--- src/edu/epam/cycles/task8/ConsistOfNumbers.java
+++ src/edu/epam/cycles/task8/ConsistOfNumbers.java
@@ -11,30 +11,63 @@ public class ConsistOfNumbers {
 
         String[] numbersPosition = {"First value", "Second value"};
         int[] numbersValue = new int[numbersPosition.length];
+        int firstSize = 0;
+        int secondSize = 0;
+        String boofer = new String("");
 
         Scanner input = new Scanner(System.in);
 
         try {
 
-            for (int counter = 0; counter < numbersPosition.length; counter ++) {
-                System.out.println("Please, enter " + numbersPosition + " : ");
-                numbersValue[counter] = input.nextInt();
+            for (int firstCounter = 0; firstCounter < numbersPosition.length; firstCounter ++) {
+
+                System.out.println("Please, enter " + numbersPosition[firstCounter] + " : ");
+                numbersValue[firstCounter] = input.nextInt();
+            }
+
+            boofer += numbersValue[0];
+            firstSize = boofer.length();
+
+            boofer += numbersValue[1];
+            secondSize = boofer.length();
+
+            int [] firstDividing = new int[firstSize];
+
+            for (int firstCounter = 0; firstCounter < firstSize; firstCounter ++) {
+
+                firstDividing[firstCounter] = numbersValue[0] % 10;
+            }
+
+            int [] secondDividing = new int[firstSize];
+
+            for (int secondCounter = 0; secondCounter < secondSize; secondCounter ++) {
+
+                secondDividing[secondCounter] = numbersValue[1] % 10;
+            }
+
+            int [] finalArray = new int[firstSize + secondSize];
+
+            System.out.println("Repeating numbers are: ");
+
+            for (int dividerFirst = 0; dividerFirst < firstSize; dividerFirst ++) {
+
+                for (int divideSecond = 0; divideSecond < secondSize; divideSecond ++) {
+
+                    if (firstDividing[firstSize] == secondDividing[secondSize]) {
+
+                        finalArray[firstSize] = firstDividing[firstSize];
+                    }
+                }
+
+                System.out.print(finalArray[firstSize] + " ");
 
             }
 
+            System.out.println(" ");
+
         } catch (InputMismatchException e) {
 
-            System.out.println(" ");
+            System.out.println("You entered wrong value. You can try again.");
         }
-
-        char[] firstNumber;
-        firstNumber = numbersValue[0].toString().toCharArray();
-
-        for (int counter2 = 0; counter2 < firstNumber.length; counter2 ++) {
-
-            System.out.print(firstNumber[counter2] + " ");
-        }
-
-        System.out.println(" ");
     }
 }
