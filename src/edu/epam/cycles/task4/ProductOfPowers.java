d01752b075fc80891a9a0ef94ae7b8c4719793ec
Tzeentche <ilyasugako87@gmail.com>
2019-07-18 22:23:59 +0300

    Added decision for task 1.3.4

---
+++ src/edu/epam/cycles/task4/ProductOfPowers.java
@@ -0,0 +1,18 @@
+package edu.epam.cycles.task4;
+
+//  Составить программу нахождения произведения квадратов первых двухсот чисел
+
+public class ProductOfPowers {
+
+    public static void main(String[] args) {
+
+        int productOfPower = 1;
+
+        for (int number = 1; number <= 200; number ++) {
+
+            productOfPower += (productOfPower * Math.pow(number, 2));
+        }
+
+        System.out.println("Product of power to 2 of first 200 numbers is " + productOfPower);
+    }
+}
